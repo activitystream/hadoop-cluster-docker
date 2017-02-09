4e312a49a0924a70e4fe4aa44eebf2a5b7974e00
Petar Shomov <petar@activitystream.com>
2017-02-09 15:44:42 +0000

    adjusting hostname and addresses

--- config/start-hadoop.sh
+++ config/start-hadoop.sh
@@ -10,6 +10,7 @@ $HADOOP_HOME/sbin/start-yarn.sh
 
 echo -e "\n"
 
-$SPARK_HOME/sbin/start-all.sh
-
+. "${SPARK_HOME}/sbin/spark-config.sh"
+SPARK_MASTER_HOST=0.0.0.0 "${SPARK_HOME}/sbin/start-master.sh"
+SPARK_MASTER_HOST=`hostname -f` "${SPARK_HOME}/sbin/start-slaves.sh"
 echo -e "\n"
