1af08a231eec5d86b38b4651b3ed2f63a880619d
Petar Shomov <petar@activitystream.com>
2017-02-11 23:12:48 +0000

    including Spark UI proxy

--- config/start-hadoop.sh
+++ config/start-hadoop.sh
@@ -11,6 +11,6 @@ $HADOOP_HOME/sbin/start-yarn.sh
 echo -e "\n"
 
 . "${SPARK_HOME}/sbin/spark-config.sh"
-SPARK_MASTER_HOST=0.0.0.0 "${SPARK_HOME}/sbin/start-master.sh"
-SPARK_MASTER_HOST=`hostname -f` "${SPARK_HOME}/sbin/start-slaves.sh"
+SPARK_LOCAL_IP=0.0.0.0 SPARK_PUBLIC_DNS="hadoop-master" "${SPARK_HOME}/sbin/start-master.sh"
+SPARK_MASTER_HOST=`hostname -f` SPARK_PUBLIC_DNS="hadoop-master" "${SPARK_HOME}/sbin/start-slaves.sh"
 echo -e "\n"
