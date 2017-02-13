9a8447bfad527714b9c9a32a9ff3cfcd05aa01d5
Petar Shomov <petar@activitystream.com>
2017-02-13 17:26:13 +0000

    building and redeploying are too separate commands

--- redeploy-cluster.sh
+++ redeploy-cluster.sh
@@ -6,9 +6,5 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 export PUSH=true
 export BUILD_REV=local
 
-cd $DIR/hadoop-base
-../build-image.sh
-cd $DIR/hadoop-master
-../build-image.sh
-cd $DIR/hadoop-node
-../build-image.sh
+cd $DIR
+docker stack rm hadoop ; docker stack deploy  --compose-file docker-compose.yml --with-registry-auth hadoop
