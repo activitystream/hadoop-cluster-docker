6d04bd260910d1d4955150fd1fc7826a08d21b48
Petar Shomov <petar@activitystream.com>
2017-02-13 17:00:50 +0000

    restructuring for clearer roles and creating the hadoop submit thing

--- build-image.sh
+++ build-image.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-echo ""
+function evil_git_dirty {
+  [[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]] && echo "D"
+}
 
-echo -e "\nbuild docker hadoop image\n"
-# docker build -t activitystream/hadoop-spark-cluster .
-docker build -t registry.activitystream.com:5043/hadoop-spark-cluster .
-docker push registry.activitystream.com:5043/hadoop-spark-cluster
+function evil_git_num_untracked_files {
+  [[ $(expr `git status --porcelain 2>/dev/null | grep "^??" | wc -l`) != "0" ]] && echo "N"
+}
+
+PUSH=${PUSH:-false}
+DIRTY1=$(evil_git_dirty)
+DIRTY2=$(evil_git_num_untracked_files)
+export BUILD_REV="`git rev-parse HEAD | cut -c 1-7`$DIRTY1$DIRTY2"
+BUILD_REV=local
+TAG="registry.activitystream.com:5043/${PWD##*/}:$BUILD_REV"
+echo -e "\nbuilding image $TAG\n"
+
+docker build -t $TAG .
+$PUSH && docker push $TAG
 
 echo "Done"
